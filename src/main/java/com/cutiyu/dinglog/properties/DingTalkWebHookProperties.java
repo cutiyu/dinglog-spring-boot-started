9c8ef33c8a7ef8927ae52cb4893439e497f4a553
tf <tangfeng@meizhu365.com>
2021-11-02 14:53:08 +0800

    发消息配置线程池

--- src/main/java/com/cutiyu/dinglog/properties/DingTalkWebHookProperties.java
+++ src/main/java/com/cutiyu/dinglog/properties/DingTalkWebHookProperties.java
@@ -4,6 +4,8 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Describe:
@@ -31,6 +33,46 @@ public class DingTalkWebHookProperties {
      */
     private String ipSegments;
 
+    private ThreadPoolProperties threadPoolProperties;
+
+    public static class ThreadPoolProperties{
+        private Integer corePoolSize;
+        private Integer maxPoolSize;
+        private Long keepAliveTime;
+        private TimeUnit timeUnit;
+
+        public Integer getCorePoolSize() {
+            return corePoolSize;
+        }
+
+        public void setCorePoolSize(Integer corePoolSize) {
+            this.corePoolSize = corePoolSize;
+        }
+
+        public Integer getMaxPoolSize() {
+            return maxPoolSize;
+        }
+
+        public void setMaxPoolSize(Integer maxPoolSize) {
+            this.maxPoolSize = maxPoolSize;
+        }
+
+        public Long getKeepAliveTime() {
+            return keepAliveTime;
+        }
+
+        public void setKeepAliveTime(Long keepAliveTime) {
+            this.keepAliveTime = keepAliveTime;
+        }
+
+        public TimeUnit getTimeUnit() {
+            return timeUnit;
+        }
+
+        public void setTimeUnit(TimeUnit timeUnit) {
+            this.timeUnit = timeUnit;
+        }
+    }
 
     public String getKeyWords() {
         return keyWords;
@@ -64,4 +106,11 @@ public class DingTalkWebHookProperties {
         this.accessToken = accessToken;
     }
 
+    public ThreadPoolProperties getThreadPoolProperties() {
+        return threadPoolProperties;
+    }
+
+    public void setThreadPoolProperties(ThreadPoolProperties threadPoolProperties) {
+        this.threadPoolProperties = threadPoolProperties;
+    }
 }
--- src/main/java/com/cutiyu/dinglog/service/impl/DingTalkSendMsgServiceImpl.java
+++ src/main/java/com/cutiyu/dinglog/service/impl/DingTalkSendMsgServiceImpl.java
@@ -3,15 +3,27 @@ package com.cutiyu.dinglog.service.impl;
 import com.cutiyu.dinglog.properties.DingTalkWebHookProperties;
 import com.cutiyu.dinglog.service.DingTalkSendMsgService;
 import com.cutiyu.dinglog.util.DingTalkSendMsgUtil;
+import com.sun.source.tree.BreakTree;
 import org.springframework.util.Assert;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Describe:
- *
  */
 public class DingTalkSendMsgServiceImpl implements DingTalkSendMsgService {
 
+
     private DingTalkWebHookProperties dingTalkWebHookProperties;
+    private ThreadPoolExecutor threadPoolExecutor;
 
 
     @Override
@@ -28,13 +40,47 @@ public class DingTalkSendMsgServiceImpl implements DingTalkSendMsgService {
 
     @Override
     public boolean sendTextMsg(String content, String accessToken, String keyWords) {
-        return DingTalkSendMsgUtil.sendTextMsg(content, accessToken, keyWords);
+        return sendTextMsgByThread(content, accessToken, keyWords);
+    }
+
+    private boolean sendTextMsgByThread(String content, String accessToken, String keyWords) {
+        Future<Boolean> submit = threadPoolExecutor.submit(new Callable<Boolean>() {
+            @Override
+            public Boolean call() throws Exception {
+                boolean succ = DingTalkSendMsgUtil.sendTextMsg(content, accessToken, keyWords);
+                return succ;
+            }
+        });
+
+        Boolean sendSucc = false;
+        try {
+            sendSucc = submit.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        return sendSucc;
     }
 
     public DingTalkSendMsgServiceImpl(DingTalkWebHookProperties dingTalkWebHookProperties) {
         this.dingTalkWebHookProperties = dingTalkWebHookProperties;
+        this.threadPoolExecutor = createThreadPoolExecutor();
     }
 
-    public DingTalkSendMsgServiceImpl() {
+    private ThreadPoolExecutor createThreadPoolExecutor() {
+        int coreSize = 10;
+        int maxSize = 10;
+        long keepAliveTime = 0L;
+        TimeUnit timeUnit = TimeUnit.SECONDS;
+        if (dingTalkWebHookProperties != null && dingTalkWebHookProperties.getThreadPoolProperties() != null) {
+            DingTalkWebHookProperties.ThreadPoolProperties poolProperties = dingTalkWebHookProperties.getThreadPoolProperties();
+            coreSize = poolProperties.getCorePoolSize() == null ? coreSize : poolProperties.getCorePoolSize();
+            maxSize = poolProperties.getMaxPoolSize() == null ? maxSize : poolProperties.getMaxPoolSize();
+            keepAliveTime = poolProperties.getKeepAliveTime() == null ? keepAliveTime : poolProperties.getKeepAliveTime();
+            timeUnit = poolProperties.getTimeUnit() == null ? timeUnit : poolProperties.getTimeUnit();
+        }
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(coreSize, maxSize, keepAliveTime, timeUnit, new LinkedBlockingQueue<Runnable>());
+        return threadPoolExecutor;
     }
 }
